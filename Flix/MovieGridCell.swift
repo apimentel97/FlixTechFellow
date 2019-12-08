8ccc9d1513d93baf429dc9010f1bb8eab9220010
Alejandro Pimentel <apimentel97@yahoo.com>
2019-12-07 18:35:03 -0800

    Week 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Alejandro pimentel on 12/7/19.
+//  Copyright © 2019 Alejandro pimentel. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
